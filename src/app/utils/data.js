2f5ff06708b00962e1d623e4d019fc33cd2b93d2
Isaac Canche <icanche83@gmail.com>
2023-02-28 11:46:34 -0600

    Cambios al endpoint /alumnos para devolver array estatico

---
+++ src/app/utils/data.js
@@ -0,0 +1,21 @@
+const alumnos = [
+    {
+        nombre: 'NombreAlumno1',
+        matricula: 'A1234567890'
+    },
+    {
+        nombre: 'NombreAlumno2',
+        matricula: 'B1234567890'
+    },
+    {
+        nombre: 'NombreAlumno3',
+        matricula: 'C1234567890'
+    },
+    {
+        nombre: 'NombreAlumno4',
+        matricula: 'D1234567890'
+    }
+]
+const profesores = []
+
+module.exports = { alumnos, profesores }
